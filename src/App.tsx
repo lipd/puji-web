8610986971ac2398fc5754e56bd2072da2e15b92
lipd <lipeidao@gmail.com>
2021-04-24 20:59:49 +0800

    chore: 引入 ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Button } from 'antd'
+import './App.less'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
 
 export default App
