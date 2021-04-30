ed74901db79dce8373dd461e2f35c6cc4c25275c
lipd <lipeidao@gmail.com>
2021-04-30 11:15:44 +0800

    feat: 完成基本乐谱渲染

--- src/components/score/index.tsx
+++ src/components/score/index.tsx
@@ -1,3 +1,41 @@
+import styled from '@emotion/styled'
+import { LegacyRef, useEffect, useRef } from 'react'
+import { OpenSheetMusicDisplay as OSMD } from 'opensheetmusicdisplay'
+import { color } from 'style/color'
+
 export const Score = () => {
-  return <div>score</div>
+  const canvasRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const osmd = new OSMD(canvasRef.current as HTMLDivElement, {
+      backend: 'svg',
+      drawTitle: true,
+      autoResize: false,
+    })
+    osmd.load('http://localhost:3000/MozaVeilSample.xml').then(() => {
+      // osmd.Sheet.Instruments[1].Visible = false
+      osmd.zoom = 0.6
+      osmd.render()
+    })
+  }, [])
+
+  return (
+    <Container>
+      <Box>
+        <div className="canvas" ref={canvasRef}></div>
+      </Box>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  min-height: 100%;
+  background-color: ${color.greyLight};
+  display: flex;
+  justify-content: center;
+`
+
+const Box = styled.div`
+  width: 70rem;
+  background: white;
+`
