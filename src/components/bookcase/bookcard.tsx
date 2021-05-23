82c47367295065b5b525df08142b95f60e0f5a13
lipd <lipeidao@gmail.com>
2021-05-23 11:09:26 +0800

    style: 改进 Bookcard 样式

--- src/components/bookcase/bookcard.tsx
+++ src/components/bookcase/bookcard.tsx
@@ -3,6 +3,9 @@ import { ReactComponent as FavoriteIcon } from 'assets/favorite.svg'
 import { ReactComponent as LikeIcon } from 'assets/like.svg'
 import { color } from 'style/color'
 import { Score } from 'types'
+import { instrumentTable } from 'utils/translate'
+
+type InstrumentTag = keyof typeof instrumentTable
 
 export const CARD_SIZE = {
   WIDTH: 32,
@@ -11,16 +14,28 @@ export const CARD_SIZE = {
 }
 
 export const Bookcard = ({ score }: { score: Score }) => {
+  const instruments = score.instruments
+    .slice(0, 4)
+    .map((each) => instrumentTable[each as InstrumentTag])
+  const instrumentNames =
+    score.instruments.length > 3 ? [...instruments, '...'] : instruments
+
   return (
     <Container>
-      <Image
-        src={`https://musescore.com/static/musescore/scoredata/g/f7c7950e4fdcbd1c7f0a7716bce8198977e39fed/score_0.png@180x252?no-cache=1611220722&bgclr=ffffff`}
-      />
+      <ImageContainer>
+        <Image src={score.cover} />
+      </ImageContainer>
       <Meta>
         <Top>
           <Title title={score.name}>{score.name}</Title>
-          <Author title={score.author}>{score.author}</Author>
-          <Instrument>{score.instruments}</Instrument>
+          <Author title={score.author}>
+            <AuthorText>{score.author}</AuthorText>
+          </Author>
+          <InstrumentGroup>
+            {instrumentNames.map((each) => (
+              <Instrument key={each}>{each}</Instrument>
+            ))}
+          </InstrumentGroup>
         </Top>
         <Bottom>
           <FavoriteIcon className="icon" />
@@ -49,46 +64,71 @@ const Container = styled.div`
   }
 `
 
-const Image = styled.img`
-  box-sizing: content-box;
-  height: 14rem;
-  width: 10rem;
+const ImageContainer = styled.div`
+  background: white;
   margin-right: 1rem;
   box-shadow: 0 1px 5px rgb(0 0 0 / 15%);
+  box-sizing: content-box;
+  padding: 0.1rem;
+`
+
+const Image = styled.img`
+  width: 9.5rem;
 `
 
 const Meta = styled.div`
   display: flex;
+  flex: 1;
+  width: 17.3rem;
+  flex-wrap: wrap;
   flex-direction: column;
+  overflow: hidden;
 `
 
 const Top = styled.div`
   flex: 1;
+  width: 100%;
 `
 
 const Title = styled.h2`
+  width: 100%;
   margin-bottom: 0.8rem;
-  height: 4rem;
+  height: 3.6rem;
   font-weight: 500;
   font-size: 1.3rem;
-  overflow: hidden;
 `
 
 const Author = styled.div`
-  padding-left: 0.5rem;
-  margin-bottom: 0.2rem;
+  margin-bottom: 0.7rem;
   width: 16rem;
   font-size: 1.2rem;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
+`
+
+const AuthorText = styled.span`
+  background: ${color.primary};
+  color: ${color.cyan};
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+`
+
+const InstrumentGroup = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 17.3rem;
+  font-size: 1.2rem;
   color: ${color.grey};
+  font-weight: 500;
 `
 
 const Instrument = styled.div`
-  padding-left: 0.5rem;
-  font-size: 1.2rem;
-  color: ${color.grey};
+  color: ${color.primary};
+  background: ${color.primaryLight};
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.4rem;
+  margin: 0.1rem;
 `
 
 const Bottom = styled.div`
@@ -97,7 +137,7 @@ const Bottom = styled.div`
   font-size: 1.2rem;
   font-weight: 500;
   color: ${color.grey};
-  padding: 0 0 1rem 0.5rem;
+  padding-left: 0.3rem;
 
   .icon {
     display: block;
