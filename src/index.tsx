0b90c1a74a992beee221161b43f49c090f994692
lipd <lipeidao@gmail.com>
2021-06-09 20:04:32 +0800

    feat: 使用 service worker 优化音源调用

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import { AppProvider } from 'context'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js')
+}
+
 ReactDOM.render(
   <AppProvider>
     <App />
